3d5d4bdd026074b1c28b49b3f1da7cdca71f08f9
Mark Kirchner <mark.kirchner4@gmail.com>
2019-05-11 20:43:34 +0200

    Umbenennen Homelist > Homes

--- HomeDings/src/de/m4rk4yy/SecretHome/cmds/Homes.java
+++ HomeDings/src/de/m4rk4yy/SecretHome/cmds/Homes.java
@@ -13,7 +13,7 @@ import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 
-public class Homelist implements CommandExecutor{
+public class Homes implements CommandExecutor{
 	
 	MethodManager mm = new MethodManager();
 
@@ -22,7 +22,7 @@ public class Homelist implements CommandExecutor{
 		Player p = (Player) cs;
 		
 		if(args.length > 0) {
-			p.sendMessage(mm.getErrorPrefix() + "Syntax: /homelist");
+			p.sendMessage(mm.getErrorPrefix() + "Syntax: /homes");
 			return true;
 		}
 		
@@ -31,12 +31,23 @@ public class Homelist implements CommandExecutor{
 			p.sendMessage(mm.getPrefix() + "Deine Homes [Klick für TP]:");
 			
 			for (String s : list) {
-				if(s.equals("home")) continue;
-
+			
+				String homename = s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
 				TextComponent prefix = new TextComponent(mm.getPrefix());
 				TextComponent msg = new TextComponent(s);
 				
-				msg.setClickEvent(new ClickEvent(Action.RUN_COMMAND, "/home " + s));
+				if(homename.equalsIgnoreCase("Home")) {
+					msg.setClickEvent(new ClickEvent(Action.RUN_COMMAND, "/home"));
+					msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT
+							, new ComponentBuilder("Klicken zum TP")
+							.color(ChatColor.DARK_AQUA).create()));
+					
+					prefix.addExtra(msg);
+					p.spigot().sendMessage(prefix);
+					return true;
+				}
+				
+				msg.setClickEvent(new ClickEvent(Action.RUN_COMMAND, "/home " + homename));
 				msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT
 						, new ComponentBuilder("Klicken zum TP")
 						.color(ChatColor.DARK_AQUA).create()));
