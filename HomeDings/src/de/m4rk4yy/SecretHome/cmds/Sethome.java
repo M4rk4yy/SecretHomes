c686780c996148a02c3e323049ee331f0f732bee
Mark Kirchner <mark.kirchner4@gmail.com>
2019-05-11 20:02:07 +0200

    Fehler bei den Permissions behoben

--- HomeDings/src/de/m4rk4yy/SecretHome/cmds/Sethome.java
+++ HomeDings/src/de/m4rk4yy/SecretHome/cmds/Sethome.java
@@ -9,78 +9,67 @@ import org.bukkit.entity.Player;
 
 import de.m4rk4yy.SecretHome.MethodManager;
 
-public class Sethome implements CommandExecutor{
+public class Sethome implements CommandExecutor {
 
 	MethodManager mm = new MethodManager();
-	
+
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
-		
+
 		Player p = (Player) cs;
-		
-		if(p.hasPermission("cmi.command.sethome.10") && !p.isOp() && !p.hasPermission("*")) {
-			int countOfHomes = mm.homeCount(p.getUniqueId());
-			if(countOfHomes >= 10) {
-				p.sendMessage(mm.getErrorPrefix() + "Maximale Anzahl von zehn Homes erreicht.");
-				return true;
-			}
+		int countOfHomes = mm.homeCount(p.getUniqueId());
+		int allowedHomes = 0;
+
+		if (p.hasPermission("cmi.command.sethome.2") && !p.isOp() && !p.hasPermission("*"))
+			allowedHomes = 2;
+		if (p.hasPermission("cmi.command.sethome.8") && !p.isOp() && !p.hasPermission("*"))
+			allowedHomes = 8;
+		if (p.hasPermission("cmi.command.sethome.10") && !p.isOp() && !p.hasPermission("*"))
+			allowedHomes = 10;
+
+		if (allowedHomes - countOfHomes == 0) {
+			p.sendMessage(mm.getErrorPrefix() + "Maximale Anzahl an Homes erreicht.");
+			return true;
 		}
-		
-		if(p.hasPermission("cmi.command.sethome.8") && !p.isOp() && !p.hasPermission("*")) {
-			int countOfHomes = mm.homeCount(p.getUniqueId());
-			if(countOfHomes >= 8) {
-				p.sendMessage(mm.getErrorPrefix() + "Maximale Anzahl von acht Homes erreicht.");
-				return true;
-			}
-		}
-		
-		if(p.hasPermission("cmi.command.sethome.2") && !p.isOp() && !p.hasPermission("*")) {
-			int countOfHomes = mm.homeCount(p.getUniqueId());
-			if(countOfHomes >= 2) {
-				p.sendMessage(mm.getErrorPrefix() + "Maximale Anzahl von zwei Homes erreicht.");
-				return true;
-			}
-		}
-		
-		if(args.length == 0) {
+
+		if (args.length == 0) {
 			Location loc = p.getLocation();
-			
+
 			World w = loc.getWorld();
 			double x = loc.getX();
 			double y = loc.getY();
 			double z = loc.getZ();
 			float yaw = loc.getYaw();
 			float pitch = loc.getPitch();
-				
+
 			mm.writeToFile(p.getUniqueId(), "home", w, x, y, z, yaw, pitch);
 			p.sendMessage(mm.getPrefix() + "Home wurde gesetzt.");
 			return true;
 		}
-		
-		if(args.length == 1 && !args[0].contains("-")) {
-			String homename = args[0].substring(0,1).toUpperCase() + args[0].substring(1).toLowerCase();
+
+		if (args.length == 1 && !args[0].contains("-")) {
+			String homename = args[0].substring(0, 1).toUpperCase() + args[0].substring(1).toLowerCase();
 			Location loc = p.getLocation();
-			
+
 			World w = loc.getWorld();
 			double x = loc.getX();
 			double y = loc.getY();
 			double z = loc.getZ();
 			float yaw = loc.getYaw();
 			float pitch = loc.getPitch();
-			
-			if(mm.nameExists(p.getUniqueId(), homename)) {
-				p.sendMessage(mm.getErrorPrefix() + "Das Home " + homename + " existiert bereits.");
-				return true;
-			}
-			
-			mm.writeToFile(p.getUniqueId(), homename, w , x, y, z, yaw, pitch);
+
+			/*
+			 * if(mm.nameExists(p.getUniqueId(), homename)) {
+			 * p.sendMessage(mm.getErrorPrefix() + "Das Home " + homename +
+			 * " existiert bereits."); return true; }
+			 */
+
+			mm.writeToFile(p.getUniqueId(), homename, w, x, y, z, yaw, pitch);
 			p.sendMessage(mm.getPrefix() + "Home " + homename + " wurde gesetzt.");
 			return true;
 		}
-		
-		
-		
-		if(args.length > 1) {
+
+		if (args.length > 1) {
 			p.sendMessage(mm.getErrorPrefix() + "Syntax: /sethome [Name]");
 			return true;
 		}
